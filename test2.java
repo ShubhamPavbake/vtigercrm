9897b267387e5900baba8284b24f00bead7488aa
ShubhamPavbake <134379894+ShubhamPavbake@users.noreply.github.com>
2023-06-04 15:54:02 +0530

    Add files via upload

---
+++ test2.java
@@ -0,0 +1,28 @@
+diff --git a/.classpath b/.classpath
+new file mode 100644
+index 0000000..e69de29
+--- /dev/null
++++ b/.classpath
+diff --git a/.project b/.project
+new file mode 100644
+index 0000000..552b964
+--- /dev/null
++++ b/.project
+@@ -0,0 +1,17 @@
++<?xml version="1.0" encoding="UTF-8"?>
++<projectDescription>
++	<name>vtigercrm</name>
++	<comment></comment>
++	<projects>
++	</projects>
++	<buildSpec>
++		<buildCommand>
++			<name>org.eclipse.jdt.core.javabuilder</name>
++			<arguments>
++			</arguments>
++		</buildCommand>
++	</buildSpec>
++	<natures>
++		<nature>org.eclipse.jdt.core.javanature</nature>
++	</natures>
++</projectDescription>
